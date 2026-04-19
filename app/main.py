bf85af494574769fe81715931786c49035c6e7c1
Neokitoss <kpokodiji13@gmail.com>
2026-04-19 22:48:07 +0300

    implement distance_class bug fix

--- app/main.py
+++ app/main.py
@@ -20,10 +20,10 @@ class Distance:
         self.km += other
         return self
 
-    def __mul__(self, other: Distance | int) -> Distance:
+    def __mul__(self, other: int) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: Distance | int) -> Distance:
+    def __truediv__(self, other: int) -> Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int) -> bool:
